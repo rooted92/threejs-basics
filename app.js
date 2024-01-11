24817398ef50d8131e54be35f9fd9ea529aabee5
Pedro Castaneda <petecast76@gmail.com>
2024-01-11 01:57:35 -0800

    created first scene

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+
+// Rendering the scene using what's called render or animate loop
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();

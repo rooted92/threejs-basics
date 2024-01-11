faa5b3d8ff8382bb1797b8b9a0f0826063ce080d
Pedro Castaneda <petecast76@gmail.com>
2024-01-11 03:24:15 -0800

    feat: made lines smaller

--- js/lines.js
+++ js/lines.js
@@ -7,7 +7,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
+camera.position.set(0, 0, 50);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
@@ -19,9 +19,9 @@ const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 // Next define geometry for lines
 
 const points = [];
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(-5, 0, 0));
+points.push(new THREE.Vector3(0, 5, 0));
+points.push(new THREE.Vector3(5, 0, 0));
 
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
